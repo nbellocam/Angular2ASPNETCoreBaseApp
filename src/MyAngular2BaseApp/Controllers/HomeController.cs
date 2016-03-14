79174d9211706ff0f7f9ba1d789a3e3670b690ad
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2016-03-13 21:31:05 -0300

    Added initial ASP.NET Core project with MVC

---
+++ src/MyAngular2BaseApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace MyAngular2BaseApp.Controllers
+{
+    using Microsoft.AspNet.Mvc;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}

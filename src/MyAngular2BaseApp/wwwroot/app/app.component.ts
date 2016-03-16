0ff4f3b156c5ec45e72022bcb6b47b4c40379e3a
Brad Rembielak <BradRembielak@live.com>
2016-03-16 11:39:28 -0400

    Allow deep linking.
    Create a server-side route to fallback to the Angular client-side routing to handle when the user types in a deep linked URL or if they refresh the page.

--- src/MyAngular2BaseApp/wwwroot/app/app.component.ts
+++ src/MyAngular2BaseApp/wwwroot/app/app.component.ts
@@ -1,7 +1,16 @@
 ﻿import {Component} from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { DeepLinkComponent } from './deepLink.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: './app/app.component.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS
+    ]
 })
+@RouteConfig([
+    { path: '/deepLink', name: 'Deep Link', component: DeepLinkComponent }
+])
 export class AppComponent { }

a4cfa4f1d62a8b9dcd7f563b23670b3fadf6d1ee
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2016-03-20 12:31:26 -0300

    Configured MVC to use camelCase for JSON

--- src/MyAngular2BaseApp/Startup.cs
+++ src/MyAngular2BaseApp/Startup.cs
@@ -9,6 +9,8 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
 
     public class Startup
     {
@@ -27,7 +29,12 @@
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Include;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

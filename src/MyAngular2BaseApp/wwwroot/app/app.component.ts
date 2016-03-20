756bda2a608a5025c75a891fe794512705419c8f
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2016-03-20 12:31:46 -0300

    Updated app component to get values from the API Controller

--- src/MyAngular2BaseApp/wwwroot/app/app.component.ts
+++ src/MyAngular2BaseApp/wwwroot/app/app.component.ts
@@ -1,7 +1,27 @@
-﻿import {Component} from 'angular2/core';
+﻿import {Component, OnInit} from "angular2/core";
+import {Http, Response} from "angular2/http";
+import 'rxjs/Rx';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    selector: "my-app",
+    template: `
+        <h1>My First Angular 2 App</h1>
+        <ul>
+          <li *ngFor="#value of values">
+           {{value}}
+          </li>
+        </ul>
+        `
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    private values: string[];
+
+    constructor(private http: Http) {
+    }
+
+    ngOnInit() {
+        return this.http.get("/api/values")
+            .map((res: Response) => <string[]>res.json())
+            .subscribe((data:string[]) => this.values = data);
+    }
+}
